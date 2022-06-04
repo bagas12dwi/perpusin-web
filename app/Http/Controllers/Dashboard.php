b96f75eb93c74dcbde75e167ba49c69dbbc2c99c
bagas12dwi <bag12dwi.s@gmail.com>
2022-06-04 23:25:48 +0700

    admin perpustakaan Done

--- app/Http/Controllers/Dashboard.php
+++ app/Http/Controllers/Dashboard.php
@@ -3,15 +3,58 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class Dashboard extends Controller
 {
     //
     public function indexAdminPerpus()
     {
-        # code...
+        $jmlBuku = DB::table('books')
+            ->join('libraries', 'libraries.id', '=', 'books.library_id')
+            ->where('libraries.user_id', auth()->user()->id)
+            ->count('books.id');
+
+        $jmlBooking = DB::table('books')
+            ->join('libraries', 'libraries.id', '=', 'books.library_id')
+            ->join('bookings', 'bookings.book_id', '=', 'books.id')
+            ->where('libraries.user_id', auth()->user()->id)
+            ->count('bookings.id');
+
+        $jmlBookingSelesai = DB::table('bookings')
+            ->join('books', 'bookings.book_id', '=', 'books.id')
+            ->join('libraries', 'books.library_id', '=', 'libraries.id')
+            ->join('users', 'users.id', '=', 'bookings.user_id')
+            ->select('bookings.id AS idBooking', 'bookings.trx_number', 'bookings.created_at', 'bookings.return_date', 'users.username', 'books.id AS idBuku', 'books.title', 'books.author', 'books.publisher', 'books.imgLocation')
+            ->where('libraries.user_id', auth()->user()->id)
+            ->where('bookings.isBack', '=', true)
+            ->count('bookings.id');
+
+        $jmlBookingBelumDikonfirmasi = DB::table('bookings')
+            ->join('books', 'bookings.book_id', '=', 'books.id')
+            ->join('libraries', 'books.library_id', '=', 'libraries.id')
+            ->join('users', 'users.id', '=', 'bookings.user_id')
+            ->select('bookings.id AS idBooking', 'bookings.trx_number', 'users.username', 'books.id AS idBuku', 'books.title', 'books.author', 'books.publisher', 'books.imgLocation')
+            ->where('libraries.user_id', auth()->user()->id)
+            ->where('bookings.full_name', '=', NULL)
+            ->count('bookings.id');
+
+        $amercements = DB::table('bookings')
+            ->join('books', 'bookings.book_id', '=', 'books.id')
+            ->join('libraries', 'books.library_id', '=', 'libraries.id')
+            ->join('users', 'users.id', '=', 'bookings.user_id')
+            ->join('amercements', 'bookings.id', '=', 'amercements.booking_id')
+            ->select('amercements.id AS idDenda', 'amercements.amercement_trx_no', 'amercements.nominal', 'amercements.updated_at', 'users.username', 'books.id AS idBuku', 'books.title', 'books.author', 'books.publisher', 'books.imgLocation')
+            ->where('libraries.user_id', auth()->user()->id)
+            ->where('bookings.isBack', '=', true)
+            ->sum('amercements.nominal');
+
         return view('adm-perpus.dashboard', [
             'title' => 'Dashboard'
-        ]);
+        ])->with('jmlBuku', $jmlBuku)
+            ->with('jmlBooking', $jmlBooking)
+            ->with('jmlBookingSelesai', $jmlBookingSelesai)
+            ->with('jmlBookingBelumDiKonfirmasi', $jmlBookingBelumDikonfirmasi)
+            ->with('denda', $amercements);
     }
 }
--- resources/views/adm-perpus/dashboard.blade.php
+++ resources/views/adm-perpus/dashboard.blade.php
@@ -1,5 +1,115 @@
 @extends('layout.adm-perpus-layout')
 
 @section('konten')
-    <h1>Dashboard</h1>
+    <div class="card shadow p-4">
+        <h5>Monitoring Data</h5>
+        <hr>
+        <div class="card-body">
+            <div class="row mb-4">
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-left-primary shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
+                                        Jumlah Buku
+                                    </div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $jmlBuku }}</div>
+                                </div>
+                                <div class="col-auto">
+                                    <i class="fas fa-fw fa-user fa-2x text-gray-300"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-left-danger shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
+                                        Jumlah Peminjaman
+                                    </div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $jmlBooking }}</div>
+                                </div>
+                                <div class="col-auto">
+                                    <i class="fas fa-fw fa-box-open fa-2x text-gray-300"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-left-success shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
+                                        Jumlah Peminjaman Selesai
+                                    </div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $jmlBookingSelesai }}</div>
+                                </div>
+                                <div class="col-auto">
+                                    <i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <a href="/konfirmasi-pembayaran" class="nav-link">
+                        <div class="card border-left-warning shadow h-100 py-2">
+                            <div class="card-body">
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col mr-2">
+                                        <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
+                                            Jumlah Peminjaman Belum Dikonfirmasi
+                                        </div>
+                                        <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                            {{ $jmlBookingBelumDiKonfirmasi }}</div>
+                                    </div>
+                                    <div class="col-auto">
+                                        <i class="fas fa-fw fa-shipping-fast fa-2x text-gray-300"></i>
+                                    </div>
+                                </div>
+                                @if ($jmlBookingBelumDiKonfirmasi > 0)
+                                    <span
+                                        class="position-absolute top-0 start-100 translate-middle p-2 bg-danger border border-light rounded-circle">
+                                        <span class="visually-hidden">New alerts</span>
+                                    </span>
+                                @else
+                                    <span
+                                        class="d-none position-absolute top-0 start-100 translate-middle p-2 bg-danger border border-light rounded-circle">
+                                        <span class="visually-hidden">New alerts</span>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <h5>Monitoring Keuangan</h5>
+            <hr>
+            <div class="row mb-4">
+                <div class="col-xl-3 col-md-6 mb-4">
+                    <div class="card border-left-danger shadow h-100 py-2">
+                        <div class="card-body">
+                            <div class="row no-gutters align-items-center">
+                                <div class="col mr-2">
+                                    <div class="text-xs font-weight-bold text-danger text-uppercase mb-1">
+                                        Jumlah Pendapatan Denda
+                                    </div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800">Rp. {{ $denda }}</div>
+                                </div>
+                                <div class="col-auto">
+                                    <i class="fas fa-wallet fa-2x text-gray-300"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
